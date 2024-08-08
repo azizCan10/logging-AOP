06c7775b39913f42fd915f20d40de998480d410f
Aziz Can Öz <oz.azizcann@gmail.com>
2024-08-08 20:25:53 +0300

    init

---
+++ src/main/java/com/test/loggingAOP/LoggingAopApplication.java
@@ -0,0 +1,13 @@
+package com.test.loggingAOP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggingAopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoggingAopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/test/loggingAOP/LoggingAopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.loggingAOP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggingAopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
